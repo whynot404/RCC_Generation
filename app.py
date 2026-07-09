303b9bbd1cac88c434144fafc1709c11528f8bb7
S4.uc3y.Be3 <japdangase@gmail.com>
2026-07-10 07:13:29 +0800

    Widgets Update
    Additional Widgets

--- app.py
+++ app.py
@@ -50,6 +50,65 @@ st.markdown("""
 
 </style>
 """, unsafe_allow_html=True)
+st.markdown("""
+<style>
+
+/* Download button */
+div[data-testid="stDownloadButton"] > button {
+
+    width: 100%;
+
+    height: 70px;
+
+    font-size: 22px;
+
+    font-weight: bold;
+
+    border-radius: 12px;
+
+}
+
+/* Generate button */
+div[data-testid="stButton"] > button {
+
+    width: 100%;
+
+    height: 60px;
+
+    font-size: 20px;
+
+    font-weight: bold;
+
+    border-radius: 12px;
+
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+def create_zip(output_files: list[Path], zip_name: str) -> Path:
+
+    zip_path = Path("Output") / zip_name
+
+    with ZipFile(zip_path, "w") as zip_file:
+
+        for file in output_files:
+            zip_file.write(file, arcname=file.name)
+
+    return zip_path
+
+st.title("ProjectRCC")
+st.write("🎉 Please upload the correct format before Running the script!")
+
+uploaded_file = st.file_uploader(
+                "Upload Consolidated RSR Schedule Summary",
+                type=["xlsx"]
+            )
+if uploaded_file:
+    df = pd.read_excel(uploaded_file)
+    st.success("File uploaded successfully!")
+    st.write(f'"Preview", number of unique outlets :{len(df)}')
+    st.dataframe(df.head())
 
 with st.sidebar:
 
@@ -96,24 +155,6 @@ Please make sure your Excel file contains the following columns:
 Contact Japs, Need niya ng Sting.
 """)
 
-def create_zip(output_files: list[Path], zip_name: str) -> Path:
-
-    zip_path = Path("Output") / zip_name
-
-    with ZipFile(zip_path, "w") as zip_file:
-
-        for file in output_files:
-            zip_file.write(file, arcname=file.name)
-
-    return zip_path
-
-st.title("ProjectRCC")
-st.write("🎉 Please upload the correct format before Running the script!")
-
-uploaded_file = st.file_uploader(
-                "Upload Consolidated RSR Schedule Summary",
-                type=["xlsx"]
-            )
 month = st.selectbox(
                 "Month",
                 range(1,13),
@@ -140,16 +181,13 @@ output_name = st.text_input(
 )       
 
 if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-    st.success("File uploaded successfully!")
-    st.write(f'"Preview", number of unique outlets :{len(df)}')
-    st.dataframe(df.head())
+    
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp:
         tmp.write(uploaded_file.getbuffer())
         temp_path = tmp.name
 
-    if st.button("Generate Schedule"):
+    if st.button("🚀 Generate Schedule"):
     
             with st.spinner('Generating Now.. Please wait.'):
                 output_files = new_generate_schedule(tempfile=temp_path,
@@ -160,13 +198,21 @@ if uploaded_file:
                 )
             zip_file = create_zip(output_files, "Generated_Schedule.zip")    
 
-            with open(zip_file, "rb") as f:
+            st.success("Schedule generated successfully!")
 
-                st.download_button(
-                    "📦 Download Schedule ZIP",
-                    data=f,
-                    file_name="Generated_Schedule.zip",
-                    mime="application/zip",
+            st.balloons()
+            st.toast("✅ 🗂️Generation Complete! Download the ZIP File.")
+            col1, col2, col3 = st.columns([1, 2, 5])
 
-                )
+            with col3:
+                with open(zip_file, "rb") as f:
+
+                    st.download_button(
+                        "📦 Download Schedule ZIP",
+                        data=f,
+                        file_name="Generated_Schedule.zip",
+                        mime="application/zip",
+
+                    )
+
 
