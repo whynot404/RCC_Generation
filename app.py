20a8d28d2afaca90030fdb897797589fcea8c3fa
S4.uc3y.Be3 <japdangase@gmail.com>
2026-07-09 16:32:34 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+from schedule_generation import new_generate_schedule
+import pandas as pd
+from zipfile import ZipFile
+from pathlib import Path
+import tempfile
+import base64
+
+
+def get_base64(file):
+    with open(file, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+bg_img = get_base64("assets/bg.jpg")
+
+st.set_page_config(
+
+    page_title="Project RCC",
+    page_icon="🐍",      # Optional: emoji or image
+    layout="centered",   # or "wide"
+    initial_sidebar_state="auto"
+
+)
+st.markdown(f"""
+<style>
+
+.stApp {{
+    background-image: url("data:image/jpeg;base64,{bg_img}");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}}
+
+</style>
+""", unsafe_allow_html=True)
+st.markdown("""
+<style>
+
+.block-container {
+    max-width: 850px;
+    margin: 100px auto 0 auto;
+    padding: 40px;
+    backdrop-filter: blur(20px);
+    background: rgba(20,20,20,.55);
+    border: 1px solid rgba(255,255,255,.15);
+    border-radius: 20px;
+    box-shadow: 0 15px 35px rgba(0,0,0,.35);
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+def create_zip(output_files: list[Path], zip_name: str) -> Path:
+
+    zip_path = Path("Output") / zip_name
+
+    with ZipFile(zip_path, "w") as zip_file:
+
+        for file in output_files:
+            zip_file.write(file, arcname=file.name)
+
+    return zip_path
+
+st.title("ProjectRCC")
+st.write("🎉 Please upload the correct format before Running the script!")
+
+uploaded_file = st.file_uploader(
+                "Upload Consolidated RSR Schedule Summary",
+                type=["xlsx"]
+            )
+month = st.selectbox(
+                "Month",
+                range(1,13),
+                index=0      # July
+            )
+
+year = st.number_input(
+    "Year",
+    min_value=2025,
+    max_value=2050,
+    value=2026
+)
+
+start_day = st.number_input(
+    "Start Day",
+    min_value=1,
+    max_value=31,
+    value=1
+)
+
+output_name = st.text_input(
+"Output filename",
+"Your_File_Name.csv"
+)       
+
+if uploaded_file:
+    df = pd.read_excel(uploaded_file)
+    st.success("File uploaded successfully!")
+    st.write(f'"Preview", number of unique outlets :{len(df)}')
+    st.dataframe(df.head())
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp:
+        tmp.write(uploaded_file.getbuffer())
+        temp_path = tmp.name
+
+    if st.button("Generate Schedule"):
+    
+            with st.spinner('Generating Now.. Please wait.'):
+                output_files = new_generate_schedule(tempfile=temp_path,
+                month=month,
+                year=year,
+                start_day=start_day,
+                output_file=output_name,
+                )
+            zip_file = create_zip(output_files, "Generated_Schedule.zip")    
+
+            with open(zip_file, "rb") as f:
+
+                st.download_button(
+                    "📦 Download Schedule ZIP",
+                    data=f,
+                    file_name="Generated_Schedule.zip",
+                    mime="application/zip",
+
+                )
+
---
+++ schedule_generation.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import time
+import calendar
+import pandas as pd
+#import os
+from pathlib import Path
+from math import ceil
+
+
+
+MAX_EXPORT_LIMIT =  100 #99_998  # Maximum number of rows recommended for export
+SCHEDULE_FILE_NAME = "sample_sched.xlsx"  # Default schedule file name
+BASE_PATH = Path(__file__).parent  # Base path of the current script
+OUTPUT_FOLDER = BASE_PATH / 'Output'
+OUTPUT_FOLDER.mkdir(exist_ok=True)
+
+
+# Export data
+def export_data(df: pd.DataFrame, file_name: str) -> list[Path]:
+    
+    len_df = len(df)
+    start = time.perf_counter()
+    output_files =[]
+   
+    if not file_name.lower().endswith(".csv"):
+        file_name += ".csv"
+
+    if len_df > MAX_EXPORT_LIMIT:
+    
+        #export_files = (len_df // MAX_EXPORT_LIMIT) + 1 
+        export_files = ceil(len_df / MAX_EXPORT_LIMIT)
+        stem = Path(file_name).stem
+
+        for i in range(export_files):
+            start_index = i * MAX_EXPORT_LIMIT
+            end_index = min((i + 1) * MAX_EXPORT_LIMIT, len_df)
+            output_path = OUTPUT_FOLDER / f"{stem}_part_{i+1}.csv"
+            
+            df.iloc[start_index:end_index].to_csv(output_path, index=False)
+            output_files.append(output_path)
+
+    else:
+        # if number of rows is less than the maximum export limit
+        output_path = OUTPUT_FOLDER / file_name
+        df.to_csv(output_path, index=False)
+        output_files.append(output_path)
+     
+    end = time.perf_counter() 
+    print(f"Total Time : {end - start:.4f} seconds")
+    return output_files
+
+# Load data from consolidated RSR
+def new_load_data(file_location)->pd.DataFrame:
+   return pd.read_excel(file_location)
+   
+def new_generate_schedule(tempfile:str,month:int, year:int,start_day:int,output_file:str)->list[Path]:
+    
+    SCHEDULE_MAIN_LIST_SET = set()
+    df = new_load_data(tempfile)
+    STORE_LIST = df.values.tolist()
+
+    last_day = calendar.monthrange(year, month)[1]
+
+    if start_day < 1 or start_day > last_day:
+        raise ValueError(
+            f"Invalid start day. Please enter a value between 1 and {last_day}."
+        )
+    for s_name in STORE_LIST:
+
+            # Service Days (e.g. Mon/Wed/Fri)
+            if pd.isna(s_name[2]):
+                continue
+            
+            service_days = str(s_name[2]).split("/")
+
+            for week in calendar.monthcalendar(year, month):
+                for day in week:
+
+                    if day == 0:
+                        continue
+
+                    # Skip dates before the selected start day
+                    if day < start_day:
+                        continue
+
+                    day_name = calendar.day_abbr[
+                        calendar.weekday(year, month, day)
+                    ]
+
+                    if day_name in service_days:
+
+                        schedule = (
+                            s_name[0],
+                            s_name[1],
+                            f"{year:04d}{month:02d}{day:02d}"
+                        )
+                    
+                        SCHEDULE_MAIN_LIST_SET.add(schedule)
+
+    df = pd.DataFrame(list(SCHEDULE_MAIN_LIST_SET),
+                      columns=[
+                          "STORE_ID",
+                          "STORE_NAME",
+                          "SERVICE_DAY"
+                      ]
+                      )
+    print(f"Generated schedules: {len(df)}")
+    print(f"Export limit: {MAX_EXPORT_LIMIT}")
+
+    return export_data(df,output_file)
+        
+
