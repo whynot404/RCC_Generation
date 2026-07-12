0f5a339f97bee6fa10b729554eb437bbcfa64ec2
S4.uc3y.Be3 <japdangase@gmail.com>
2026-07-12 09:09:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
     
 if "username" not in st.session_state:
-    st.session_state.username = None
+    st.session_state.username = ""
 #----------------- LOGIN END -----------------#
 
 #----------------- FUNCTIONS -----------------#
